f4c0cfe22c5da2b745a0f54d1219cefcb3d7d790
Alberto <arobld00@gmail.com>
2019-06-01 22:41:50 +0200

    modificada query

--- application/models/Account_model.php
+++ application/models/Account_model.php
@@ -6,13 +6,20 @@ class Account_model extends CI_Model {
 	public function cargarPedidos($data) {
 		$this->db->select('u.nombre, u.apellidos, p.num_ped, p.fecha, p.estado, f.precio');
 		$this->db->from('Pedido p, Forma f, Usuario u');
-		$this->db->where("f.id_usu = p.id_usu AND p.id_usu = u.id AND f.id_usu = '" . $data['id_usu'] . "'");
-		$this->db->group_by('p.num_ped');
+		$this->db->where("f.num_ped = p.num_ped AND p.id_usu = u.id AND f.id_usu = '" . $data['id_usu'] . "'");
+		$this->db->order_by('p.fecha');
 		$pedidos = $this->db->get();
 		if ($pedidos->num_rows() > 0) {
 			return $pedidos->result();
 		} else {
 			return false;
+		}
+	}
+
+	public function address_insert($data) {
+		$this->db->insert('dir_usuario', $data);
+		if ($this->db->affected_rows() > 0) {
+			return true;
 		}
 	}
 }
